a59839667033f7c5873d6a0e423fcea557a6c64e
VisionTEK <info@visiontek.co.za>
2024-06-29 14:42:58 +0200

    config update

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    server_url: 'http://visiontek.ddns.net',
+    server_url: 'Server URL',
     sql_server_url: 'localhost',
     webPort: 8001,
     apiPort: 8002,
     logging: true,
     api_debug: true,
     clearLogsOnStart: true,  // Set this to true or false to control archiving and clearing of logs
-    dbUser: 'VisionTEK',
-    dbPassword: 'b!lls4w5yqYN]Wsb',
-    dbName: 'dashee2_0',
+    dbUser: 'Database Username',
+    dbPassword: 'Database Password',
+    dbName: 'Database Name',
     sqlPort: 3306,
     uploadConfig: {
         maxFileSize: 50 * 1024 * 1024, // 50MB
         allowedFormats: ['image/jpeg', 'image/png', 'image/webp', 'image/jpg', 'video/mp4', 'video/avi']
     },
-    secretKey: 'q8i570tovb1u439dlt101nhv0mire6omvq3wk5prz652zj5sd70bsz7aukdt', // used for token generation
-    adminToken: '3v3fb7h2haosp1xyz6jica3iz400xbuco5o96lbose6o19akuf1aicwnr7oy',
+    secretKey: 'API secret key', // used for token generation
+    adminToken: 'API admin token',
     support_request_max_file_size_mb: 10,
     support_request_file_formats: 'png,jpg,jpeg,webp,mp4,avi',
     g_site_key: 'your-secret-key',
